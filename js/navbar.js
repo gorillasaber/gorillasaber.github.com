d816b0f0208944b50b0636d598973ce05fd5d9d0
vanvani41 <iberezuk523@gmail.com>
2026-04-26 23:39:12 +0300

    asd

--- js/navbar.js
+++ js/navbar.js
@@ -1,16 +1,18 @@
-const me = JSON.parse(localStorage.getItem("discord_me") || "null");
-let loginBtn = document.getElementById("discordLoginBtn");
+(() => {
+  const me = JSON.parse(localStorage.getItem("discord_me") || "null");
+  const loginBtn = document.getElementById("discordLoginBtn");
 
-if (loginBtn) {
-  if (me) {
-    loginBtn.style.display = "none";
-    loginBtn.insertAdjacentHTML("afterend",
-      `<a href="#" id="logoutBtn" style="color:#e57373;">Вийти (${me.username})</a>`
-    );
-    document.getElementById("logoutBtn").addEventListener("click", () => {
-      localStorage.removeItem("discord_me");
-      localStorage.removeItem("discord_token");
-      location.reload();
-    });
+  if (loginBtn) {
+    if (me) {
+      loginBtn.style.display = "none";
+      loginBtn.insertAdjacentHTML("afterend",
+        `<a href="#" id="logoutBtn" style="color:#e57373;">Вийти (${me.username})</a>`
+      );
+      document.getElementById("logoutBtn").addEventListener("click", () => {
+        localStorage.removeItem("discord_me");
+        localStorage.removeItem("discord_token");
+        location.reload();
+      });
+    }
   }
-}
+})();
